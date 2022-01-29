84dc2a0b6e947b009f1daa48e65ee73397315515
Ionut Jula <ionut.jula@emerson.com>
2022-01-29 22:52:42 +0200

    Update JS

--- script.js
+++ script.js
@@ -6,10 +6,36 @@
 // Modal
 
 get_countries();
+const countries_el = document.getElementById('countries');
 
-async function get_countries() {
+
+async function get_countries()
+{
     const res = await fetch('https://restcountries.com/v3.1/all');
     const countries = await res.json();
 
     console.log(countries);
+    display_countries(countries);
+}
+
+function display_countries(countries)
+{
+    for (let i = 0; i < countries.length; i++) {
+        const country_el = document.createElement('div');
+        country_el.classList.add('card');
+    
+        country_el.innerHTML = `
+            <div class="card-header">
+                <img src="${countries[i].flags.svg}" alt="Peru">
+            </div>
+            <div class="card-body">
+                <h2>${countries[i].name.common}</h2>
+                <p><strong>Population: </strong>${countries[i].population}</p>
+                <p><strong>Region: </strong>${countries[i].region}</p>
+                <p><strong>Capital: </strong>${countries[i].capital}</p>
+            </div>
+        `;
+        countries_el.appendChild(country_el);
+    }
+    
 }
